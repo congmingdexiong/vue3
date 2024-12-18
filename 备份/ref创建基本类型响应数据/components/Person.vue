69a451c20ec2c762fe9664adc9b80a6a9786a4cf
aleng1106 <ltlobygg@163.com>
2024-12-18 15:37:53 +0800

    ref create basic reactive data

---
+++ 备份/ref创建基本类型响应数据/components/Person.vue
@@ -0,0 +1,38 @@
+﻿<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script setup lang="ts" name="Person">
+import { ref } from 'vue'
+const name = ref('张三')
+const age = ref(19)
+const tel = '138888888'
+
+function changeName() {
+  name.value = 'zhang-san'
+}
+function changeAge() {
+  age.value += 1
+}
+function showTel() {
+  alert(tel)
+}
+</script>
+
+<style>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 0 5px;
+}
+</style>
