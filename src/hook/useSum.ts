1b863c8006cbd28f11d49eed6c839102fa79d7e2
aleng1106 <ltlobygg@163.com>
2024-12-25 14:44:26 +0800

    029_自定义hook

---
+++ src/hook/useSum.ts
@@ -0,0 +1,20 @@
+﻿import { ref, reactive, computed } from 'vue'
+import axios from 'axios'
+
+export default function () {
+  const sum = ref(0)
+
+  const bigSum = computed(() => {
+    return sum.value * 1000
+  })
+
+  function add() {
+    sum.value += 1
+  }
+
+  return {
+    sum,
+    add,
+    bigSum,
+  }
+}
