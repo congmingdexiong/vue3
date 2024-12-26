60a13dc03fd005a6c05b3166bb69a9a1b8dc40d4
aleng1106 <ltlobygg@163.com>
2024-12-26 11:19:14 +0800

    040_路由_replace_属性

---
+++ 备份/040_路由_replace_属性/pages/Detail.vue
@@ -0,0 +1,18 @@
+﻿<!-- Vue3 template -->
+<template>
+  <ul>
+    <li>编号:{{ id }}</li>
+    <li>标题:{{ title }}</li>
+    <li>内容:{{ content }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="Detail">
+defineProps({
+  id: Number,
+  title: String,
+  content: String,
+})
+</script>
+
+<style scoped></style>
