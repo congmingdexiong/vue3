11e831e63bbf79e60d252bcde484589d906e6caa
aleng1106 <ltlobygg@163.com>
2024-12-26 09:07:13 +0800

    037_路由_query参数

---
+++ 备份/037_路由_query参数/router/index.ts
@@ -0,0 +1,29 @@
+﻿import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import News from '@/pages/News.vue'
+import Detail from '@/pages/Detail.vue'
+
+const routes = createRouter({
+  // history: createWebHashHistory(),
+  history: createWebHistory(),
+  routes: [
+    { name: 'zhuye', path: '/', component: () => Home },
+    { name: 'zhuye1', path: '/home', component: () => Home },
+    { name: 'guanyu', path: '/about', component: () => About },
+    {
+      name: 'xinwen',
+      path: '/news',
+      component: () => News,
+      children: [
+        {
+          path: 'detail',
+          component: () => Detail,
+        },
+      ],
+    },
+    // { name: '404', path: '/:pathMatch(.*)*', component: () => Home },
+  ],
+})
+
+export default routes
